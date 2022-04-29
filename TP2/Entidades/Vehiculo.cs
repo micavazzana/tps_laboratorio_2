eed0bd04f435936a21d21b96bb4ca57093e69980
Mica Vazzana <micaelavazzana@hotmail.com>
2022-04-29 14:04:18 -0300

    tp2 falta revisar

---
+++ TP2/Entidades/Vehiculo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
+        }
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+
+        private EMarca marca;
+        private string chasis;
+        private ConsoleColor color;
+
+        protected Vehiculo(EMarca marca, string chasis, ConsoleColor color)
+        {
+            this.marca = marca;
+            this.chasis = chasis;
+            this.color = color;
+        }
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected abstract ETamanio Tamanio { get; }
+
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"CHASIS: {p.chasis}\r\n");
+            sb.AppendLine($"MARCA : {p.marca}\r\n");//to string?
+            sb.AppendLine($"COLOR : {p.color}\r\n");//to string?
+            sb.AppendLine("---------------------");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return v1.chasis == v2.chasis;
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+    }
+}
